f7ad35cc110fee20f6d736841024721b0ff007ab
Jericho <huelarjl18@gmail.com>
2024-01-28 03:08:28 -0700

    2.2 text processing

--- Lab 1/part2.6.py
+++ Lab 1/part2.6.py
@@ -10,3 +10,4 @@ print(result)
 
 execution_time = timeit.timeit(lambda: pow2(10000), number=10000)
 print("Time it takes to execute 10000 instances of pow2(10000):", execution_time)
+
--- Lab 1/textprocessing.py
+++ Lab 1/textprocessing.py
@@ -1,35 +1,20 @@
+def vowel_checker(word, vowels): # function to check vowels in a text file
+    return sum(char in vowels for char in word) # checks the list of vowels and checks the character in a word if it includes a vowel
 
-def vowel_checker(word) #checks and iterates over a word for a vowel. If a vowel, is found add +1 to the vowel counter
+word_counter = 0 # word counter used in avg numbers of vowels per word
+vowel_counter = 0 # vowel counter used in avg numbers of vowels per word
+vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'] # list of vowels used in vowel checker
+
+with open(r'pg2701.txt', 'r', encoding='utf-8') as file: #opening the file 
+        for i, line in enumerate(file, 1):
+            if i >= 41:     # starting the read at "CHAPTER 1. Loomings."
+                words = line.split()
+                word_counter += len(words)
+                for word in words:
+                    vowel_counter += vowel_checker(word.lower(), vowels)
+
+
+average_vowels_per_word = vowel_counter / word_counter     
+
+print(f'Average vowels per word starting from line 41: {average_vowels_per_word:.2f}')
     
-    return any(char in vowels for char in word)
-
-
-#create a loop or something to make file read starting at line 41
-with open(r'pg2701.txt', 'r') as file:
-    word_counter = 0
-    vowel_counter = 0
-    vowels = ['a','A','e','E','i','I','o','O','u','U','y','Y']
-
-
-     for i, line in enumerate(file.readlines(), 0):
-          if i >= 41
-            words = line.split()
-            for word in words:
-                if vowel_checker(word):
-                    vowel_counter += 1
-
-
-    data = file.read()
-    lines = data.split()
-
-
-        
-        
-        # Iterate through the words in each line
-        
-
-
-
-#output
-    average_vowels_per_word = vowel_counter / word_counter
-    print(f"Average vowels per word: {average_vowels_per_word:.2f}")
