8544fa403d3676549b16187057c9353ab4e1eaeb
Seanybobocoy <sean.sabo@ucalgary.ca>
2024-01-29 15:47:13 -0700

    Fixed more .py files.

--- Lab 1/ex2.5.py
+++ Lab 1/ex2.5.py
@@ -7,12 +7,12 @@ def modify_records(data):
             record['payload']['size'] = 42
 
 def load_data(input_file):
-    with open(input_file, 'r') as file:
+    with open(input_file, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 def save_data(data, output_file):
     data.reverse()
-    with open(output_file, 'w') as file:
+    with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
 def main(input_file, output_file):
@@ -26,7 +26,7 @@ def main(input_file, output_file):
 
     save_data(data, output_file)
 
-input_file_path = 'large-file.json'
+input_file_path = 'Lab 1/large-file.json'
 output_file_path = 'output.2.3.json'
 
 main(input_file_path, output_file_path)
